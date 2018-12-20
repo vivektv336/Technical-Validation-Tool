7240b0fd1a7284dbbc1f227103cd928454663948
vivektv336 <46020537+vivektv336@users.noreply.github.com>
2018-12-20 15:04:12 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace testproj2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\geckodriver\");
+            //var options = new FirefoxDriver();
+            //options.SetPreference("webdriver.gecko.driver", @"D:\geckodriver.exe");
+            //IWebDriver driver = new FirefoxDriver();
+            //IWebDriver driver = new FirefoxDriver(@"C:\driver"); 
+            IWebDriver driver = new FirefoxDriver(service);
+            driver.Navigate().GoToUrl("https://technical-validation-web.ausvdc02.pcf.dell.com/");
+        }
+    }
+}
